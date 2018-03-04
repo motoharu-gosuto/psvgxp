1d6b9dfa5638d557c53e4ea7c0ce2a0cc7d17831
motoharu-gosuto <motoharu.gosuto@gmail.com>
2018-03-04 20:47:24 +0300

    add some bruteforcing code

--- psvgxp/BruteForceTests.cpp
+++ psvgxp/BruteForceTests.cpp
@@ -348,3 +348,224 @@ void enumerate_0x08000000_0x10000000_swizzles(const SceGxmProgram* header, ScePs
       }
    }
 }
+
+void bruteforce_0x18000000_0x20000000_strange(ScePsp2ShaderPerfOptions* opt, const SceGxmProgram* header)
+{
+   std::uint8_t swz_en_strange1 = 0;
+   std::uint8_t swz_en_strange0 = 1;
+   for(std::uint8_t swz_mask3 = 0; swz_mask3 <= 1; swz_mask3++)
+   {
+      for(std::uint8_t swz_mask2 = 0; swz_mask2 <=1; swz_mask2++)
+      {
+         for(std::uint8_t swz_mask1 = 0; swz_mask1 <=1; swz_mask1++)
+         {
+            for(std::uint8_t swz_en = 0; swz_en <=1; swz_en++)
+            {
+               //std::uint64_t instr = INSTR_0x18000000_0x20000000_DOT(3,0,0,0,0,0,0,0,0,0,0,0,swz_en_strange1,swz_en_strange0,0,swz_mask3,swz_mask2,swz_mask1,swz_en,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+               //std::uint64_t instr = INSTR_0x18000000_0x20000000_DOT(3,0,0,0,0,0,1,0,1,0,0,0,swz_en_strange1,swz_en_strange0,0,swz_mask3,swz_mask2,swz_mask1,swz_en,0,0,0,0,0,1,1,0,8,0,0,0,0,0,0,0,0,8);
+               std::uint64_t instr = INSTR_0x18000000_0x20000000_DOT(3,0,1,1,0,1,0,1,0,1,1,0,swz_en_strange1,swz_en_strange0,1,swz_mask3,swz_mask2,swz_mask1,swz_en,0,0,1,1,1,0,0,0,0,0,0,1,1,1,1,0);
+               disasm_gxp_explicit(opt, header, instr);
+            }
+         }
+      }
+   }
+}
+
+void bruteforce_0x18000000_0x20000000_swz(ScePsp2ShaderPerfOptions* opt, const SceGxmProgram* header)
+{
+   std::uint8_t unk12 = 1;
+   std::uint8_t unk11 = 1;
+   std::uint8_t unk9 = 1;
+   std::uint8_t unk8 = 1;
+   std::uint8_t unk7 = 1;
+   std::uint8_t unk6 = 1;
+   std::uint8_t unk5 = 1;
+   std::uint8_t unk4 = 1;
+   std::uint8_t unk3 = 1;
+
+   std::uint8_t swz_en_strange1 = 0;
+   std::uint8_t swz_en_strange0 = 1;
+
+   std::uint8_t c3_en = 1;
+
+   for(std::uint8_t swz_alt_op2 = 0; swz_alt_op2 <= 3; swz_alt_op2++)
+   {
+      for(std::uint8_t op2_swz = 0; op2_swz <= 3; op2_swz++)
+      {
+         std::uint64_t instr = INSTR_0x18000000_0x20000000_DOT(3,0,unk12,unk11,0,c3_en,1,unk9,1,unk8,unk7,0,swz_en_strange1,swz_en_strange0,
+                                                               unk6,1,1,1,1,0,0,unk5,unk4,unk3,1,1,0,0x8,swz_alt_op2,op2_swz,0,0,0,0,8);
+         disasm_gxp_explicit(opt, header, instr);
+      }
+   }
+}
+
+void bruteforce_0x00000000_0x08000000_swizzles1()
+{
+   /*
+   std::uint8_t swz_mask16 = 0;
+   //for(std::uint8_t swz_mask16 = 0; swz_mask16 <= 1; swz_mask16++)
+   //{
+      for(std::uint8_t swz_mask32 = 0; swz_mask32 <= 1; swz_mask32++)
+      {
+         for(std::uint8_t swz_en = 0; swz_en <= 1; swz_en++)
+         {
+            std::uint64_t instr = INSTR_0x00000000_0x08000000(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,swz_mask16,swz_mask32,swz_en,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+            disasm_gxp_explicit(&opt, &header, instr);
+         }
+      }
+   //}
+   */
+}
+
+void bruteforce_0x00000000_0x08000000_swizzles2()
+{
+   /*
+   std::uint8_t swz_mask16 = 0;
+   std::uint8_t swz_mask32 = 0;
+   std::uint8_t swz_en = 1;
+
+   std::uint8_t swz_alt_op1 = 1;
+   std::uint8_t swz_alt_op2 = 0;
+   std::uint8_t swz_alt_op3 = 0;
+   std::uint8_t op1_swz = 3;
+   std::uint8_t op2_swz = 0;
+   std::uint8_t op3_swz = 0;
+
+   std::uint64_t instr = INSTR_0x00000000_0x08000000(0,0,0,0,0,swz_alt_op1,0,0,0,0,0,swz_alt_op3,op3_swz,swz_alt_op2,0,0,swz_mask16,swz_mask32,swz_en,0,0,0,0,0,0,0,0,0,op2_swz,op1_swz,0,0,0);
+   disasm_gxp_explicit(&opt, &header, instr);
+   */
+}
+
+void bruteforce_0x00000000_0x08000000_index()
+{
+   /*
+   for(std::uint8_t op0 = 0; op0 <= 0x3F; op0++)
+   {
+      std::uint64_t instr = INSTR_0x00000000_0x08000000(0,0,0,0,1,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,2,0,0,op0,0,0,0,0,0);
+      disasm_gxp_explicit(&opt, &header, instr);
+   }
+   */
+}
+
+void bruteforce_0x00000000_0x08000000_constants()
+{
+   //op0 mode 32 constants
+   /*
+   for(std::uint8_t op0 = 0; op0 <= 0x3F; op0++)
+   {
+      std::uint64_t instr = INSTR_0x00000000_0x08000000(0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,0,0,op0,0,0,0,0,0);
+      disasm_gxp_explicit(&opt, &header, instr);
+   }
+   */
+
+   /*
+   //op0 mode 16 constants
+   for(std::uint8_t op0 = 0; op0 <= 0x3F; op0++)
+   {
+      std::uint64_t instr = INSTR_0x00000000_0x08000000(0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,1,0,0,op0,0,0,0,0,0);
+      disasm_gxp_explicit(&opt, &header, instr);
+   }
+   */
+
+   //op2 mode 32 constants
+   /*
+   for(std::uint8_t op2 = 0; op2 <= 0x3F; op2++)
+   {
+      std::uint64_t instr = INSTR_0x00000000_0x08000000(0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,0,0,0,0,0,op2,0);
+      disasm_gxp_explicit(&opt, &header, instr);
+   }
+   */
+}
+
+void bruteforce_0x08000000_0x10000000_opcode_pred()
+{
+   /*
+   //opcodes with predicates
+   for(std::uint8_t predicate = 0; predicate <=7; predicate++)
+   {
+      for(std::uint8_t opcode2 = 0; opcode2 <=7; opcode2++)
+      {
+         std::uint64_t instr = INSTR_0x08000000_0x10000000(1,predicate,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,opcode2,0,0);
+         disasm_gxp_explicit(&opt, &header, instr);
+      }
+   }
+  */
+}
+
+void bruteforce_0x10000000_0x18000000_opcode_pred()
+{
+   /*
+   //opcodes with predicates
+   for(std::uint8_t predicate = 0; predicate <=7; predicate++)
+   {
+      for(std::uint8_t opcode2 = 0; opcode2 <=7; opcode2++)
+      {
+         std::uint64_t instr = INSTR_0x10000000_0x18000000(2,predicate,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,opcode2,0,0);
+         disasm_gxp_explicit(&opt, &header, instr);
+      }
+   }
+   */
+}
+
+void bruteforce_0x18000000_0x20000000_mad_masking()
+{
+    //masking
+   /*
+   for(std::uint8_t swz_mask3 = 0;swz_mask3 <=1; swz_mask3++)
+   {
+      for(std::uint8_t swz_mask2 = 0;swz_mask2 <=1; swz_mask2++)
+      {
+         for(std::uint8_t swz_mask1 = 0;swz_mask1 <=1; swz_mask1++)
+         {
+            for(std::uint8_t swz_en = 0;swz_en <=1; swz_en++)
+            {
+               std::uint64_t instr = INSTR_0x18000000_0x20000000_MAD(3,0,0,0,1,0,0,0,0,0,0,0,0,0,0,swz_mask3,swz_mask2,swz_mask1,swz_en,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+               disasm_gxp_explicit(&opt, &header, instr);
+            }
+         }
+      }
+   }
+   */
+}
+
+void bruteforce_0x18000000_0x20000000_mad_swizzles()
+{
+   /*
+   for(std::uint8_t swz_alt_op1 = 0; swz_alt_op1 <=7; swz_alt_op1++)
+   {
+      for(std::uint8_t op1_swz = 0; op1_swz <=3; op1_swz++)
+      {
+         std::uint64_t instr = INSTR_0x18000000_0x20000000_MAD(3,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,swz_alt_op1,op1_swz,0);
+         disasm_gxp_explicit(&opt, &header, instr);
+      }
+   }
+   */
+   
+   /*
+   for(std::uint8_t swz_alt_op2_2 = 0; swz_alt_op2_2 <=1; swz_alt_op2_2++)
+   {
+      for(std::uint8_t swz_alt_op2_x = 0; swz_alt_op2_x <=3; swz_alt_op2_x++)
+      {
+         for(std::uint8_t op2_swz = 0; op2_swz <= 3; op2_swz++)
+         {
+            std::uint64_t instr = INSTR_0x18000000_0x20000000_MAD(3,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,swz_alt_op2_2,0,0,0,0,swz_alt_op2_x,op2_swz,0,0,0,0,0,0,0);
+            disasm_gxp_explicit(&opt, &header, instr);
+         }
+      }
+   }
+   */
+
+   /*
+   for(std::uint8_t swz_alt_op3_2 = 0; swz_alt_op3_2 <=1; swz_alt_op3_2++)
+   {
+      for(std::uint8_t swz_alt_op3_x = 0; swz_alt_op3_x <=3; swz_alt_op3_x++)
+      {
+         for(std::uint8_t op3_swz = 0; op3_swz <=3; op3_swz++)
+         {
+            std::uint64_t instr = INSTR_0x18000000_0x20000000_MAD(3,0,0,swz_alt_op3_2,1,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,swz_alt_op3_x,op3_swz,0,0,0,0,0);
+            disasm_gxp_explicit(&opt, &header, instr);
+         }
+      }
+   }
+   */
+}
